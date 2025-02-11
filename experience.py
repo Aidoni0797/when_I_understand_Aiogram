fabe4f62f7fc33acc8fbfe827e47e9c4625f86ac
iDONi <30952073+Aidoni0797@users.noreply.github.com>
2025-02-11 08:24:03 +0600

    Such an experience, ugh

---
+++ experience.py
@@ -0,0 +1,60 @@
+import requests
+import json
+
+
+token = "..."
+username = "..."
+
+
+BOT_TOKEN = '...'
+CHAT_ID = '...'
+
+
+if not token:
+    raise ValueError("Токен GitHub не найден. Убедитесь, что он добавлен в .env файл.")
+
+# Получаем список репозиториев пользователя
+repos_url = f"https://api.github.com/users/{username}/repos"
+headers = {'Authorization': f'token {token}'}
+response = requests.get(repos_url, headers=headers)
+
+if response.status_code != 200:
+    raise Exception(f"Ошибка при получении репозиториев: {response.status_code} - {response.json()}")
+
+repos = response.json()
+result = {}
+rre=[]
+# Для каждого репозитория получаем статистику
+for repo in repos:
+    repo_name = repo['name']
+    traffic_url = f"https://api.github.com/repos/{username}/{repo_name}/traffic/clones"
+    traffic_response = requests.get(traffic_url, headers=headers)
+
+    if traffic_response.status_code != 200:
+        print(f"Ошибка при получении статистики для {repo_name}: {traffic_response.status_code}")
+        continue
+
+    traffic_data = traffic_response.json()
+
+    if 'clones' in traffic_data:
+        last_clone = traffic_data['clones'][-1] if traffic_data['clones'] else None
+        if last_clone:
+            result['message'] = f"*{repo_name}: {last_clone['timestamp']}"
+            url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+            params = {
+                'chat_id': CHAT_ID,
+                'text': result['message']
+            }
+            response = requests.get(url, params=params)
+        else:
+            result['message'] = f"Repo: {repo_name} has no cloning data."
+
+    else:
+        result['message'] = f"Repo: {repo_name} - no traffic data available."
+
+
+
+
+
+
+
